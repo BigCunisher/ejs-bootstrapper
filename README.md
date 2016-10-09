e236ea74d5cd42faa08a02abd4fcb8501f279bd4
BigCunisher <mcunliffe3@gmail.com>
2016-10-09 22:41:14 +0100

    Added capability to define custom head content

--- README.md
+++ README.md
@@ -9,4 +9,11 @@ To stand up the application:
 
 To create content for your site add a directory structure and an **_.ejs_** file containing a new pages content to the **_/client/views/includes/content/_** directory.
 
- To render any added content visit the corresponding URL structure, so for example if you add **_/foo/bar.ejs_** to the **_/client/views/includes/content/_** directory structure, the URL to render this content would be http://localhost:8080/foo/bar
+New content is added by convention. To add a new page to the application, for example **http://localhost:8080/foo/bar**:
+
+1. Add **_/foo/bar.ejs_** to **_/client/views/includes/content_**
+2. Populate **_/foo/bar.ejs_** with your desired HTML mark-up
+3. Add **_/foo/barHead.ejs_** to **_/client/views/includes/head_**
+4. Populate **_/foo/barHead.ejs_** with custom <head> content for the new page
+
+Pages are cached by default for 5 minutes and therefore changes can take up to 5 minutes to take affect. Set the `cacheTimeout` and the `cacheInterval` variable in the **_routes.js_** to `0` to set an unlimited cache timeout
--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,46 @@
 var fs = require('fs');
+var NodeCache = require('node-cache');
+
+var cacheTimeout = 300;
+var cacheInterval = 100;
+
+var urlCache = new NodeCache({ stdTTL: cacheTimeout, checkperiod: cacheInterval });
+var notFoundCache = new NodeCache({ stdTTL: cacheTimeout, checkperiod: cacheInterval });
+var contentPath = '/client/views/includes/content';
+var contentSuffix = '.ejs';
+var headPath = '/client/views/includes/head';
+var headSuffix = 'Head.ejs';
+var template = '/client/views/index.ejs';
+
 module.exports = function(app, rootDir, frontEnd) {
+
   app.all('*', function(request, response){
-    fs.exists(rootDir + '/client/views/includes/content' + generateDynamicPath(request.path) + '.ejs', function(exists) {
-      if(exists){
-        response.render(rootDir + '/client/views/index.ejs', {contentName:  generateDynamicPath(request.path)});
+      if(doesResourceExist(rootDir + contentPath + generateDynamicPath(request.path) + contentSuffix, request.path) && doesResourceExist(rootDir + headPath + generateDynamicPath(request.path) + headSuffix, request.path)){
+        if(!doesCacheEntryExist(urlCache, request.path)) urlCache.set(request.path, request.path, function(err, success){
+          if(err) console.log(request.path + ' failed to be added to URL cache');
+        });
+        response.render(rootDir + template, {contentName:  generateDynamicPath(request.path)});
       } else {
+        if(!doesCacheEntryExist(notFoundCache, request.path)) notFoundCache.set(request.path, request.path, function(err, success){
+          if(err) console.log(request.path + ' failed to be added to 404 cache');
+        });
         response.sendStatus(404);
       }
-    });
   });
+
   function generateDynamicPath(path){
     return path == '/' ? '/home' : path;
+  }
+
+  function doesResourceExist(path, uri){
+    return !doesCacheEntryExist(notFoundCache, uri) && (doesCacheEntryExist(urlCache, uri) || fs.existsSync(path));
+  }
+
+  function doesCacheEntryExist(cache, key){
+    try{
+      return cache.get(key, true) !== 'undefined';
+    } catch(err){
+      return false;
+    }
   }
 }
